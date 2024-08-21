50324db54157034ffe6e29332eadf5fc2282f371
artemtsvietkov <143085970+artemtsvietkov@users.noreply.github.com>
2024-08-21 10:46:01 +0200

    done

--- main.py
+++ main.py
@@ -5,11 +5,11 @@
 from random import randint
 
 def main():
-    user_input = input().split(" ")
+    user_input = [int(x) for x in input("").split(" ")]
     max_value = int(max(user_input))
     min_value = int(min(user_input))
 
-    for x in range(min_value + 1, max_value +2):
+    for x in range(min_value + 1, max_value + 2):
         print(x)
 if __name__ == "__main__":
     main()
